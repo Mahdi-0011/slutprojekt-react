c694667620e9b5a74cb8ed4a3340186a146c3cb4
Mahdi <mahdi.mousavi0011@gmail.com>
2025-04-17 22:53:40 +0200

    fix: add navbar to header and better style for logo.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,19 +1,16 @@
 // import { Link } from "react-router";
+import Navbar from "../navbar/navbar"
 import "./Header.scss"
 
-// const navLink = [
-//   {name:"home", url:"/", id:"1"},
-//   {name:"favorites", url:"/favorites", id:"2"},
-// ];
-{/* <nav>
-      {navLink.map((link) => (<Link to={link.url} key={link.id} className="navlink">{link.name}</Link>))}
-      </nav> */}
+
 export default function Header() {
   return (
     <>
     <header className="header">
+    <div className="logo-container">
     <div className="logo">logo</div>
-    <nav className="navbar">navbar</nav>
+    </div>
+    <nav className="navbar"><Navbar/></nav>
     </header>
     </>
   )
