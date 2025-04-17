a6b7c8c6ca110d638e0460ea65a333c091095110
Mahdi <mahdi.mousavi0011@gmail.com>
2025-04-17 16:34:26 +0200

    fix: add flex styles for logo and navbar in header.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,26 +1,20 @@
-import { Link } from "react-router";
-import Input from "../UI/Input/input"
+// import { Link } from "react-router";
 import "./Header.scss"
 
-const navLink = [
-  {name:"home", url:"/", id:"1"},
-  {name:"favorites", url:"/favorites", id:"2"},
-];
+// const navLink = [
+//   {name:"home", url:"/", id:"1"},
+//   {name:"favorites", url:"/favorites", id:"2"},
+// ];
+{/* <nav>
+      {navLink.map((link) => (<Link to={link.url} key={link.id} className="navlink">{link.name}</Link>))}
+      </nav> */}
 export default function Header() {
   return (
     <>
-    <div 
-      className="header"> 
-      header
-      <nav>
-        {navLink.map((link) => (<Link to={link.url} key={link.id} className="navlink">{link.name}</Link>))}
-      </nav>
-      <div 
-      className="logo">
-      logo
-    </div>
-      <Input />
-    </div>
+    <header className="header">
+    <div className="logo">logo</div>
+    <nav className="navbar">navbar</nav>
+    </header>
     </>
   )
 }
