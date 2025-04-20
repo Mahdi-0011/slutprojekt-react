c85f7e8589b2501d408fae3889e85e8d060dd536
Mahdi <mahdi.mousavi0011@gmail.com>
2025-04-20 16:22:20 +0200

    add input in home page.

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,13 +1,21 @@
+import Card from "../../components/card/card"
 import "./home.scss"
 
 export default function Home() {
   return (
     <>
-    <div className="homepage">Home</div>
-    <p>this is my homepage</p>
-    <div className="card"></div>
-    <div className="card"></div> 
-    <div className="card"></div>
+    <div className="homepage">
+      <form action="input" className="input-form">
+        <input type="text" />
+        <button>search</button>
+      </form>
+    </div>
+    <div className="card-show">
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    </div>
     </>
   )
 }
