48fab58f5cadc8c5311e1131d57f5c9777afeab7
Mahdi <mahdi.mousavi0011@gmail.com>
2025-04-21 19:10:02 +0200

    add input to home page and a search btn.

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
 import Card from "../../components/card/card"
 import "./home.scss"
 
+
+const Search = (e: { preventDefault: () => void; }) =>{
+  e.preventDefault();
+};
+
+
 export default function Home() {
+  const [searchText, setSearchText] = useState("");
   return (
     <>
     <div className="homepage">
       <form action="input" className="input-form">
-        <input type="text" />
-        <button>search</button>
+        <input type="text" 
+        placeholder="search your book ..."
+        onChange={(e)=>{
+          setSearchText(e.target.value)}}
+        />
+        <button onClick={Search}>search</button>
+        <p>u wrote: {searchText}</p>
       </form>
     </div>
     <div className="card-show">
