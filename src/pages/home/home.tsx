fa08f7765930b9743a5f8cd359027609c5390375
Mahdi <mahdi.mousavi0011@gmail.com>
2025-04-22 21:02:45 +0200

    add fetch data exampel or test.

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Card from "../../components/card/card";
 import "./home.scss";
+import DataFetcher from "../../servies/api";
 
 const Search = (e: { preventDefault: () => void }) => {
   e.preventDefault();
@@ -25,9 +26,7 @@ export default function Home() {
       </div>
       <div className="card-show">
         <Card />
-        <Card />
-        <Card />
-        <Card />
+        <DataFetcher />
       </div>
     </>
   );
--- src/servies/api.tsx
+++ src/servies/api.tsx
@@ -1,3 +1,60 @@
-export default function api() {
-  return <div>api</div>;
+import { useEffect, useState } from 'react';
+
+type BookProps = {
+  author_key: string[],
+  author_name: string[],
+  cover_edition_key: string,
+  cover_i: number,
+  edition_count: number,
+  first_publish_year: number,
+  has_fulltext: boolean,
 }
+
+const DataFetcher = () => {
+  const [data, setData] = useState<BookProps[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://openlibrary.org/search.json?q=the+lord+of+the+rings');
+      if (!response.ok) {
+        throw new Error('Network response was not ok.');
+      }
+      const data = await response.json();
+      setData(data.docs);
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+      } else {
+        setError("An unknown error occurred.");
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>Error: {error}</p>
+      ) : (
+        <ul>
+          {data.map((item) => (
+            <li key={item.cover_edition_key}>
+              {item.author_name}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default DataFetcher;
