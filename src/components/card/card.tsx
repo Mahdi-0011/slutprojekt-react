9de0ce8fe0461c9c12e196e0520e11ca11780197
Mahdi <mahdi.mousavi0011@gmail.com>
2025-04-24 15:09:21 +0200

    add book-pic to card component.

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,9 +1,16 @@
 import "./card.scss";
 
 type Book = {
-  title: string;
+  cover_i:            number;
+  has_fulltext:       boolean;
+  edition_count:      number;
+  title:              string;
+  author_name:        string[];
   first_publish_year: number;
-  key: string;
+  key:                string;
+  ia:                 string[];
+  author_key:         string[];
+  public_scan_b:      boolean;
 };
 
 type BookCardProps = {
@@ -16,6 +23,14 @@ const Card: React.FC<BookCardProps> = ({ book }) => {
       <button>❤️</button>
       <p><strong>Title:</strong> {book.title}</p>
       <p><strong>First published:</strong> {book.first_publish_year}</p>
+      <p><strong>your fulltext:</strong>{book.public_scan_b}</p>
+      <img 
+      src={book.cover_i 
+       ? `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg` 
+      : "/images/placeholder.jpg"} 
+      alt="books-img" 
+      className="book-image" 
+      />
     </div>
   );
 };
