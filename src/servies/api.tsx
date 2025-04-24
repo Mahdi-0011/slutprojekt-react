09189a96937cdaf5c36ee4388b510293d5455501
Mahdi <mahdi.mousavi0011@gmail.com>
2025-04-24 12:00:38 +0200

    fix: use searchTerm in api.

--- src/servies/api.tsx
+++ src/servies/api.tsx
@@ -1,30 +1,32 @@
 import { useEffect, useState } from 'react';
 import Card from '../components/card/card';
 import "./api.scss"
-
+type searchTermProps = {
+  searchTerm: string;
+}
 type BookProps = {
   title: string;
   first_publish_year: number;
   key: string;
 };
 
-const DataFetcher = () => {
+const DataFetcher = ({searchTerm}: searchTermProps) => {
   const [data, setData] = useState<BookProps[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [searchTerm]);
 
   const fetchData = async () => {
     try {
-      const response = await fetch('https://openlibrary.org/search.json?q=${query}');
+      const response = await fetch(`https://openlibrary.org/search.json?q=${encodeURIComponent(searchTerm)}&sort=title`);
       if (!response.ok) {
         throw new Error('Network response was not ok.');
       }
       const data = await response.json();
-      setData(data.docs.slice(0,10));
+      setData(data.docs.slice(0, 30));
     } catch (error) {
       if (error instanceof Error) {
         setError(error.message);
