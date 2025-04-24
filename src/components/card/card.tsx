be5888090e9a73cc442cd89a76567aee2c25cf4b
Mahdi <mahdi.mousavi0011@gmail.com>
2025-04-24 11:59:10 +0200

    add btn favorite and icon.

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -13,6 +13,7 @@ type BookCardProps = {
 const Card: React.FC<BookCardProps> = ({ book }) => {
   return (
     <div className="card">
+      <button>❤️</button>
       <p><strong>Title:</strong> {book.title}</p>
       <p><strong>First published:</strong> {book.first_publish_year}</p>
     </div>
