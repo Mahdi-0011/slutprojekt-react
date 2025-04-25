35e4e785c66765dfa306409af16d0990672214d2
Mahdi <mahdi.mousavi0011@gmail.com>
2025-04-25 11:24:13 +0200

    fix: add better style for cards.

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -20,17 +20,23 @@ type BookCardProps = {
 const Card: React.FC<BookCardProps> = ({ book }) => {
   return (
     <div className="card">
-      <button>❤️</button>
-      <p><strong>Title:</strong> {book.title}</p>
-      <p><strong>First published:</strong> {book.first_publish_year}</p>
-      <p><strong>Author name:</strong>{book.author_name}</p>
-      <img 
+            <img 
       src={book.cover_i 
        ? `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg` 
       : "/images/placeholder.jpg"} 
       alt="books-img" 
       className="book-image" 
       />
+      <br />
+      <br />
+      <button className="favorite-btn">❤️</button>
+      <br />
+      <br />
+      <h4>Title: {book.title}</h4>
+      <br />
+      <p><strong>First published:</strong> {book.first_publish_year}</p>
+      <br />
+      <p><strong>Author name:</strong>{book.author_name}</p>
     </div>
   );
 };
