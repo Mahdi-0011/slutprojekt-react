68d3e98bdd98315583a5f2059c859b0942b959a8
Mahdi <mahdi.mousavi0011@gmail.com>
2025-04-25 11:23:19 +0200

    add BookList compnent.

---
+++ src/components/book-list/book-list.tsx
@@ -0,0 +1,32 @@
+import Card from "../card/card";
+import "./book-list.scss"
+
+
+type BookProps = {
+  cover_i: number;
+  has_fulltext: boolean;
+  edition_count: number;
+  title: string;
+  author_name: string[];
+  first_publish_year: number;
+  key: string;
+  ia: string[];
+  author_key: string[];
+  public_scan_b: boolean;
+};
+
+type BookListProps = {
+  books: BookProps[];
+};
+
+const BookList = ({ books }: BookListProps) => {
+  return (
+    <div className="card-grid">
+      {books.map((book) => (
+        <Card key={book.key} book={book} />
+      ))}
+    </div>
+  );
+};
+
+export default BookList;
