3885e0449a05aa8715267660711172a6d7153779
Mahdi <mahdi.mousavi0011@gmail.com>
2025-04-28 20:31:00 +0200

    add reducer function to the app.

---
+++ src/Reducer/FavoriteReducer.tsx
@@ -0,0 +1,26 @@
+type StateType = {
+  Favorites: number[];
+}
+type ActionType = {
+  type: "add to favorite" |
+        "remove favorite" ;
+  payload: number;
+}
+
+
+export function Reducer(state: StateType, action: ActionType) {
+  switch (action.type) {
+    case 'add to favorite': {
+      return {
+        ...state, Favorites: [...state.Favorites, action.payload]
+      };
+    }
+    case 'remove favorite': {
+      return {
+        ...state, Favorites: (state.Favorites.filter(Favorite => Favorite !== action.payload))
+      };
+    }
+    default:
+    throw Error('Unknown action: ' + action.type);
+  }
+}
