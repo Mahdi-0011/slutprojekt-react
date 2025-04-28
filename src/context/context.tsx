5f02fd4715aaef6a6876a61e492e077745ffa72a
Mahdi <mahdi.mousavi0011@gmail.com>
2025-04-28 20:31:36 +0200

    add contex for favorite cards.

--- src/context/context.tsx
+++ src/context/context.tsx
@@ -1,3 +1,31 @@
-export default function context() {
-  return <></>;
+import { createContext, useReducer } from "react";
+import { Reducer } from "../Reducer/FavoriteReducer";
+
+
+
+const GlobalContext = createContext<favorite[]>("");
+
+type initialStateType = {
+  favorite: [];
 }
+const initialState: initialStateType = {
+  favorite: [],
+};
+
+type ChildrenType = {
+  children: React.ReactNode;
+}
+export default function ContextProvider({ children } : ChildrenType) {
+  const [state, dispatch] = useReducer(Reducer, initialState)
+
+
+  return (
+    <GlobalContext.Provider value={ state, dispatch }>
+      {children}
+    </GlobalContext.Provider>
+  );
+}
+
+
+
+
