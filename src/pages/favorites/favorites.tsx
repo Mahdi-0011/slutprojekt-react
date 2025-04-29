8f22c87ca762e83f2686e8dde9d9e85a74ce5fbd
Mahdi <mahdi.mousavi0011@gmail.com>
2025-04-29 13:22:48 +0200

    test my favorite page with globalcontext.

--- src/pages/favorites/favorites.tsx
+++ src/pages/favorites/favorites.tsx
@@ -1,10 +1,15 @@
+
 import "./favorites.scss";
+import useGlobalContext from "../../customHooks/useGlobalContext";
 
 export default function Favorites() {
+  const { state } = useGlobalContext();
   return (
     <>
       <div>favorites</div>
       <p>this is my favoritespage</p>
+      <p>{state.Favorites}</p>
     </>
   );
 }
+
