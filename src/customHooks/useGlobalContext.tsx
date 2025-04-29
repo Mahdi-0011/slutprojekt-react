9b2ad949a9d947c966d46b0768a7f04c1e27246b
Mahdi <mahdi.mousavi0011@gmail.com>
2025-04-29 13:20:29 +0200

    add customhook for context.

---
+++ src/customHooks/useGlobalContext.tsx
@@ -0,0 +1,11 @@
+import { useContext } from "react";
+import { GlobalContext } from "../context/context";
+
+
+export default function useGlobalContext() {
+  const context = useContext(GlobalContext);
+  if (!context) {
+    throw new Error("useGlobalContext must be used within a ContextProvider");
+  }
+  return context;
+}
