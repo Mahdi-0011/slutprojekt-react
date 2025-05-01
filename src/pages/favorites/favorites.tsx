aefbf2df1dc01a17557cde274e2fd47a17b0752e
Mahdi <mahdi.mousavi0011@gmail.com>
2025-05-01 03:10:28 +0200

    Add favorite cards to favorites page with basic styling and delete functionality.

--- src/pages/favorites/favorites.tsx
+++ src/pages/favorites/favorites.tsx
@@ -1,42 +1,49 @@
 import "./favorites.scss";
 import useGlobalContext from "../../customHooks/useGlobalContext";
+import { BookType } from "../../Types/BookType";
 
 export default function Favorites() {
-  const { state } = useGlobalContext();
+  const { state, dispatch } = useGlobalContext();
+
+  const handleRemove = (book: BookType) => {
+    dispatch({ type: "remove favorite", payload: book });
+  };
 
   return (
-    <>
-      <div className="favorites-page">
-        <h2>Favorites</h2>
-        <p>This is my favorites page</p>
-
-        {state.Favorites.length === 0 ? (
-          <p>No favorites yet 😢</p>
-        ) : (
-          <div className="favorites-list">
-            {state.Favorites.map((book) => (
-              <div className="favorite-card" key={book.cover_i}>
-                <img
-                  src={
-                    book.cover_i
-                      ? `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
-                      : "/images/placeholder.jpg"
-                  }
-                  alt="book-cover"
-                  className="book-image"
-                />
-                <h4>{book.title}</h4>
-                <p>
-                  <strong>Author:</strong> {book.author_name?.join(", ")}
-                </p>
-                <p>
-                  <strong>First published:</strong> {book.first_publish_year}
-                </p>
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
-    </>
+    <div className="favorites-page">
+      {state.Favorites.length === 0 ? (
+        <p>No favorites yet 😢</p>
+      ) : (
+        <div className="favorites-list">
+          {state.Favorites.map((book) => (
+            <div className="favorite-card" key={book.cover_i}>
+              <img
+                src={
+                  book.cover_i
+                    ? `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
+                    : "/images/placeholder.jpg"
+                }
+                alt="book-cover"
+                className="book-image"
+              />
+              <br />
+              <br />
+              <button onClick={() => handleRemove(book)}>Remove</button>
+              <br />
+              <br />
+              <h4>{book.title}</h4>
+              <br />
+              <p>
+                <strong>Author:</strong> {book.author_name?.join(", ")}
+              </p>
+              <br />
+              <p>
+                <strong>First published:</strong> {book.first_publish_year}
+              </p>
+            </div>
+          ))}
+    </div>
+      )}
+    </div>
   );
 }
