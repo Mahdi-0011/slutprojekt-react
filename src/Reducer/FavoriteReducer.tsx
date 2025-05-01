cb34c3e6c2985feb601bfcc2b1824f364a30cc59
Mahdi <mahdi.mousavi0011@gmail.com>
2025-05-01 03:04:20 +0200

    move reducer to a new folder.

--- src/Reducer/FavoriteReducer.tsx
+++ src/Reducer/FavoriteReducer.tsx
@@ -1,25 +1,32 @@
-// type StateType = {
-//   Favorites: number[];
-// }
-// type ActionType = {
-//   type: "add to favorite" |
-//         "remove favorite" ;
-//   payload: number;
-// }
 
-// export function Reducer(state: StateType, action: ActionType) {
-//   switch (action.type) {
-//     case 'add to favorite': {
-//       return {
-//         ...state, Favorites: [...state.Favorites, action.payload]
-//       };
-//     }
-//     case 'remove favorite': {
-//       return {
-//         ...state, Favorites: (state.Favorites.filter(Favorite => Favorite !== action.payload))
-//       };
-//     }
-//     default:
-//     throw Error('Unknown action: ' + action.type);
-//   }
-// }
+import { BookType } from "../Types/BookType";
+
+export type StateType = {
+  Favorites: BookType[];
+};
+export type ActionType = {
+  type: "add to favorite" | "remove favorite";
+  payload: BookType;
+};
+
+
+export function Reducer(state: StateType, action: ActionType) {
+  switch (action.type) {
+    case "add to favorite": {
+      return {
+        ...state,
+        Favorites: [...state.Favorites, action.payload],
+      };
+    }
+    case "remove favorite": {
+      return {
+        ...state,
+        Favorites: state.Favorites.filter(
+          (Favorite) => Favorite.cover_i !== action.payload.cover_i,
+        ),
+      };
+    }
+    default:
+      throw Error("Unknown action: " + action.type);
+  }
+}
