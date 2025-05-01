b34d06b21d6aaea99b1b0c53a882dc1f9231d8e5
Mahdi <mahdi.mousavi0011@gmail.com>
2025-05-01 03:12:22 +0200

    move reducer from context file.

--- src/context/context.tsx
+++ src/context/context.tsx
@@ -1,45 +1,18 @@
 /** @format */
 
 import { createContext, useReducer } from "react";
-import { BookType } from "../Types/BookType";
+import { ActionType, Reducer, StateType } from "../Reducer/FavoriteReducer";
 
-type StateType = {
-  Favorites: BookType[];
-};
-type ActionType = {
-  type: "add to favorite" | "remove favorite";
-  payload: BookType;
-};
 
-const initialState: StateType = {
-  Favorites: [],
-};
 
 type ContextType = {
   state: StateType;
   dispatch: React.Dispatch<ActionType>;
 };
+const initialState: StateType = {
+  Favorites: [],
+};
 
-export function Reducer(state: StateType, action: ActionType) {
-  switch (action.type) {
-    case "add to favorite": {
-      return {
-        ...state,
-        Favorites: [...state.Favorites, action.payload],
-      };
-    }
-    case "remove favorite": {
-      return {
-        ...state,
-        Favorites: state.Favorites.filter(
-          (Favorite) => Favorite.cover_i !== action.payload.cover_i,
-        ),
-      };
-    }
-    default:
-      throw Error("Unknown action: " + action.type);
-  }
-}
 
 export const GlobalContext = createContext<ContextType | undefined>(undefined);
 
