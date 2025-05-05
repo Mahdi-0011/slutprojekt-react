147fdfb5bab87d29280a6e974f714cc610234efe
Mahdi <mahdi.mousavi0011@gmail.com>
2025-05-05 13:20:26 +0200

    add checkbox- rating, review, amount inputs and save, delete, edite bttuons to favorite page. add little syle for them too.

--- src/pages/favorites/favorites.tsx
+++ src/pages/favorites/favorites.tsx
@@ -9,8 +9,15 @@ export default function Favorites() {
     dispatch({ type: "remove favorite", payload: book });
   };
 
+  type HandleCheckboxOnChange = () => void;
+  const handleCheckboxOnChange: HandleCheckboxOnChange = () => {};
+  
   return (
     <div>
+      <div>
+        <h4>Books you read: {"lord of the rings, hello"}___how many books:{5}</h4>
+        <h4>pages you read: {5}</h4>
+      </div>
         <div className="favorites-list">
           {state.Favorites.map((book) => (
             <div className="favorite-card" key={book.cover_i}>
@@ -23,20 +30,43 @@ export default function Favorites() {
                 alt="book-cover"
                 className="book-image"
               />
-              <br />
-              <br />
-              <button onClick={() => handleRemove(book)}> 💔 </button>
-              <br />
-              <br />
               <h4>{book.title}</h4>
-              <br />
               <p>
                 <strong>Author:</strong> {book.author_name?.join(", ")}
               </p>
-              <br />
               <p>
                 <strong>First published:</strong> {book.first_publish_year}
               </p>
+              <button 
+              onClick={() => handleRemove(book)}
+              className="favorite-btn"
+              > 💔 </button>
+              <div>
+              <div>
+              Read:
+              <input type="checkbox" 
+              onChange={handleCheckboxOnChange}
+              />
+              </div>
+              <div>
+              <input type="number"
+              placeholder=" Rating ..."
+              className="rating-input" />
+              </div>
+              <div>
+              <input type="number"
+              placeholder=" Amount ..."
+              className="numberofpages-inout" />
+              </div>
+              <div>
+              <input type="text"
+              placeholder=" Review ..."
+              className="review-inout" />
+              </div>
+              <button>save</button>
+              <button>Delete</button>
+              <button>Edite</button>
+              </div>
             </div>
           ))}
     </div>
